cf88fefb705250d0570fb8c1ba0513a5e2073a82
Markwill2099 <69679676+Markwill2099@users.noreply.github.com>
2023-07-01 01:04:56 -0300

    Update CollectionImpl.java
    Últimas atualizações.

--- src/Projetofinal_QuizCtF/CollectionImpl.java
+++ src/Projetofinal_QuizCtF/CollectionImpl.java
@@ -2,3 +2,9 @@ package Projetofinal_QuizCtF;
 
 abstract class CollectionImpl<E> implements Collection<E> {
 }
+
+// A classe abstrata chamada CollectionImpl que implementa a interface Collection<E>. A classe possui um parâmetro de tipo genérico E, que representa o tipo dos elementos contidos na coleção. 
+
+// A palavra-chave abstract indica que a classe é abstrata, o que significa que ela não pode ser instanciada diretamente, mas pode ser estendida por outras classes.
+
+//A classe CollectionImpl está implementando a interface Collection<E>, o que significa que ela deve fornecer implementações para todos os métodos declarados na interface.
