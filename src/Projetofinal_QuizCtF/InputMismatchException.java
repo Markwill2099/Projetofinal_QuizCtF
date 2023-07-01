420f8cecc97406563306a8cb115c1d3cfa8d0f2e
Markwill2099 <69679676+Markwill2099@users.noreply.github.com>
2023-07-01 01:14:24 -0300

    Update InputMismatchException.java
    Últimas atualizações.

--- src/Projetofinal_QuizCtF/InputMismatchException.java
+++ src/Projetofinal_QuizCtF/InputMismatchException.java
@@ -1,10 +1,15 @@
 package Projetofinal_QuizCtF;
 
-import java.util.NoSuchElementException;
+import java.util.NoSuchElementException; // A classe NoSuchElementException é uma exceção que é lançada quando um elemento solicitado não pode ser encontrado.
 
 
     public class InputMismatchException extends NoSuchElementException {
+        // A classe InputMismatchException que estende a classe NoSuchElementException. Isso significa que InputMismatchException herda todos os métodos e comportamentos da classe NoSuchElementException.
+
+        // A constante estática serialVersionUID do tipo long é usada para controlar a serialização da classe InputMismatchException.
         private static final long serialVersionUID = 8811230760997066428L;
+
+
 
         /**
          * Constructs an <code>InputMismatchException</code> with <tt>null</tt>
@@ -13,6 +18,7 @@ import java.util.NoSuchElementException;
         public InputMismatchException() {
             super();
         }
+        // Construtor padrão da classe InputMismatchException. Ele chama o construtor da classe pai (NoSuchElementException) para inicializar a exceção sem uma mensagem de erro específica.
 
         /**
          * Constructs an <code>InputMismatchException</code>, saving a reference
@@ -24,5 +30,6 @@ import java.util.NoSuchElementException;
         public InputMismatchException(String s) {
             super(s);
         }
+        // Construtor adicional da classe InputMismatchException. Ele recebe uma mensagem de erro como argumento e chama o construtor da classe pai (NoSuchElementException) passando essa mensagem para inicializar a exceção com uma mensagem específica.
     }
 
